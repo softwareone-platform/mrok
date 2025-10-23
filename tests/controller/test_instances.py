e0f84cefdbecc8082d8a3be0d1b6209c3a776618
Aleksandra Ovchinnikova <nsanjka@gmail.com>
2025-10-23 09:21:31 -0400

    MPT-14717 mrok add identities to controller router

--- tests/controller/test_instances.py
+++ tests/controller/test_instances.py
@@ -3,52 +3,45 @@ from urllib.parse import quote
 import pytest
 from httpx import AsyncClient
 from pytest_httpx import HTTPXMock
-from pytest_mock import MockerFixture
 
-from mrok.ziti.api import ZitiClientAPI, ZitiManagementAPI
 from mrok.ziti.constants import MROK_SERVICE_TAG_NAME, MROK_VERSION_TAG_NAME
 from tests.conftest import SettingsFactory
 
 
 @pytest.mark.asyncio
 async def test_list_instances(
-    mocker: MockerFixture,
     api_client: AsyncClient,
     settings_factory: SettingsFactory,
     httpx_mock: HTTPXMock,
 ):
     settings = settings_factory()
-    mocker.patch(
-        "mrok.controller.routes.fetch_extension_or_404", return_value={"name": "ext-1234-5678"}
-    )
-    query = quote(f'tags.{MROK_SERVICE_TAG_NAME} = "ext-1234-5678"')
     httpx_mock.add_response(
         method="GET",
-        url=f"{settings.ziti.api.management}/edge/management/v1/identities?filter={query}&limit=5&offset=0",
+        url=f"{settings.ziti.api.management}/edge/management/v1/identities?&limit=10&offset=0",
         json={
-            "meta": {"pagination": {"totalCount": 10, "limit": 5, "offset": 0}},
-            "data": [{"id": f"ins{i}", "name": "ins.svc"} for i in range(5)],
+            "meta": {"pagination": {"totalCount": 15, "limit": 10, "offset": 0}},
+            "data": [{"id": f"ins{i}", "name": "ins.svc"} for i in range(10)],
         },
     )
     httpx_mock.add_response(
         method="GET",
-        url=f"{settings.ziti.api.management}/edge/management/v1/identities?filter={query}&limit=5&offset=5",
+        url=f"{settings.ziti.api.management}/edge/management/v1/identities?&limit=10&offset=10",
         json={
-            "meta": {"pagination": {"totalCount": 10, "limit": 5, "offset": 5}},
-            "data": [{"id": f"ins{5 + i}", "name": "ins.svc"} for i in range(5)],
+            "meta": {"pagination": {"totalCount": 15, "limit": 10, "offset": 10}},
+            "data": [{"id": f"ins{i}", "name": "ins.svc"} for i in range(11, 16)],
         },
     )
-    response = await api_client.get("/extensions/ext-1234-5678/instances?limit=5&offset=0")
+    response = await api_client.get("/instances?limit=10&offset=0")
     assert response.status_code == 200
     page_1 = response.json()
-    assert len(page_1["data"]) == 5
+    assert len(page_1["data"]) == 10
     assert page_1["$meta"]["pagination"]["offset"] == 0
 
-    response = await api_client.get("/extensions/ext-1234-5678/instances?limit=5&offset=5")
+    response = await api_client.get("/instances?limit=10&offset=10")
     assert response.status_code == 200
     page_2 = response.json()
     assert len(page_2["data"]) == 5
-    assert page_2["$meta"]["pagination"]["offset"] == 5
+    assert page_2["$meta"]["pagination"]["offset"] == 10
 
     assert sorted(page_1["data"], key=lambda x: x["id"]) != sorted(
         page_2["data"], key=lambda x: x["id"]
@@ -56,69 +49,14 @@ async def test_list_instances(
 
 
 @pytest.mark.asyncio
-async def test_register_instance(mocker: MockerFixture, api_client: AsyncClient):
-    mocker.patch(
-        "mrok.controller.routes.fetch_extension_or_404", return_value={"name": "ext-1234-5678"}
-    )
-    mocked_register = mocker.patch(
-        "mrok.controller.routes.register_instance",
-        return_value=(
-            {
-                "id": "a1b2cd",
-                "name": "ins-1234-5678-0001.ext-1234-5678",
-                "tags": {
-                    MROK_VERSION_TAG_NAME: "0.0.0.dev0",
-                    MROK_SERVICE_TAG_NAME: "ext-1234-5678",
-                    "account": "ACC-1234-5678",
-                },
-            },
-            {
-                "identity": "json",
-            },
-        ),
-    )
-
-    response = await api_client.post(
-        "/extensions/ext-1234-5678/instances",
-        json={
-            "instance": {"id": "INS-1234-5678-0001"},
-            "tags": {"account": "ACC-1234-5678"},
-        },
-    )
-    assert response.status_code == 201
-    assert response.json() == {
-        "id": "a1b2cd",
-        "extension": {"id": "EXT-1234-5678"},
-        "instance": {"id": "INS-1234-5678-0001"},
-        "name": "ins-1234-5678-0001.ext-1234-5678",
-        "tags": {
-            MROK_VERSION_TAG_NAME: "0.0.0.dev0",
-            MROK_SERVICE_TAG_NAME: "ext-1234-5678",
-            "account": "ACC-1234-5678",
-        },
-        "identity": {"identity": "json"},
-    }
-    assert mocked_register.call_count == 1
-    assert isinstance(mocked_register.mock_calls[0].args[0], ZitiManagementAPI)
-    assert isinstance(mocked_register.mock_calls[0].args[1], ZitiClientAPI)
-    assert mocked_register.mock_calls[0].args[2] == "ext-1234-5678"
-    assert mocked_register.mock_calls[0].args[3] == "INS-1234-5678-0001"
-    assert mocked_register.mock_calls[0].args[4] == {"account": "ACC-1234-5678"}
-
-
-@pytest.mark.asyncio
 async def test_get_instance(
-    mocker: MockerFixture,
     settings_factory: SettingsFactory,
     api_client: AsyncClient,
     httpx_mock: HTTPXMock,
 ):
-    mocker.patch(
-        "mrok.controller.routes.fetch_extension_or_404", return_value={"name": "ext-1234-5678"}
-    )
     settings = settings_factory()
     query = quote(
-        '(id="INS-1234-5678-0001.ext-1234-5678" or name="ins-1234-5678-0001.ext-1234-5678") '
+        '(id="INS-1234-1234-0001.ext-1234-1234" or name="ins-1234-1234-0001.ext-1234-1234") '
         f"and tags.{MROK_VERSION_TAG_NAME} != null"
     )
     httpx_mock.add_response(
@@ -129,40 +67,37 @@ async def test_get_instance(
             "data": [
                 {
                     "id": "ins1",
-                    "name": "ins-1234-5678-0001.ext-1234-5678",
+                    "name": "ins-1234-1234-0001.ext-1234-1234",
                     "tags": {
                         MROK_VERSION_TAG_NAME: "0.0.0.dev0",
-                        MROK_SERVICE_TAG_NAME: "ext-1234-5678",
+                        MROK_SERVICE_TAG_NAME: "ext-1234-1234",
                     },
                 }
             ],
         },
     )
 
-    response = await api_client.get("/extensions/EXT-1234-5678/instances/INS-1234-5678-0001")
+    response = await api_client.get("/instances/INS-1234-1234-0001.ext-1234-1234")
     assert response.status_code == 200
     assert response.json() == {
         "id": "ins1",
-        "extension": {"id": "EXT-1234-5678"},
-        "instance": {"id": "INS-1234-5678-0001"},
-        "name": "ins-1234-5678-0001.ext-1234-5678",
+        "identity": None,
+        "extension": {"id": "EXT-1234-1234"},
+        "instance": {"id": "INS-1234-1234-0001"},
+        "name": "ins-1234-1234-0001.ext-1234-1234",
         "tags": {
             MROK_VERSION_TAG_NAME: "0.0.0.dev0",
-            MROK_SERVICE_TAG_NAME: "ext-1234-5678",
+            MROK_SERVICE_TAG_NAME: "ext-1234-1234",
         },
     }
 
 
 @pytest.mark.asyncio
 async def test_get_instance_by_instance_id(
-    mocker: MockerFixture,
     settings_factory: SettingsFactory,
     api_client: AsyncClient,
     httpx_mock: HTTPXMock,
 ):
-    mocker.patch(
-        "mrok.controller.routes.fetch_extension_or_404", return_value={"name": "ext-1234-5678"}
-    )
     settings = settings_factory()
     query = quote(f'(id="ins1" or name="ins1") and tags.{MROK_VERSION_TAG_NAME} != null')
     httpx_mock.add_response(
@@ -173,43 +108,40 @@ async def test_get_instance_by_instance_id(
             "data": [
                 {
                     "id": "ins1",
-                    "name": "ins-1234-5678-0001.ext-1234-5678",
+                    "name": "ins-1234-1234-0001.ext-1234-1234",
                     "tags": {
                         MROK_VERSION_TAG_NAME: "0.0.0.dev0",
-                        MROK_SERVICE_TAG_NAME: "ext-1234-5678",
+                        MROK_SERVICE_TAG_NAME: "ext-1234-1234",
                     },
                 }
             ],
         },
     )
 
-    response = await api_client.get("/extensions/EXT-1234-5678/instances/ins1")
+    response = await api_client.get("/instances/ins1")
     assert response.status_code == 200
     assert response.json() == {
         "id": "ins1",
-        "extension": {"id": "EXT-1234-5678"},
-        "instance": {"id": "INS-1234-5678-0001"},
-        "name": "ins-1234-5678-0001.ext-1234-5678",
+        "extension": {"id": "EXT-1234-1234"},
+        "instance": {"id": "INS-1234-1234-0001"},
+        "name": "ins-1234-1234-0001.ext-1234-1234",
         "tags": {
             MROK_VERSION_TAG_NAME: "0.0.0.dev0",
-            MROK_SERVICE_TAG_NAME: "ext-1234-5678",
+            MROK_SERVICE_TAG_NAME: "ext-1234-1234",
         },
+        "identity": None,
     }
 
 
 @pytest.mark.asyncio
 async def test_get_instance_not_found(
-    mocker: MockerFixture,
     settings_factory: SettingsFactory,
     api_client: AsyncClient,
     httpx_mock: HTTPXMock,
 ):
-    mocker.patch(
-        "mrok.controller.routes.fetch_extension_or_404", return_value={"name": "ext-1234-5678"}
-    )
     settings = settings_factory()
     query = quote(
-        '(id="INS-1234-5678-0001.ext-1234-5678" or name="ins-1234-5678-0001.ext-1234-5678") '
+        '(id="INS-1234-1234-0001.ext-1234-1234" or name="ins-1234-1234-0001.ext-1234-1234") '
         f"and tags.{MROK_VERSION_TAG_NAME} != null"
     )
     httpx_mock.add_response(
@@ -221,51 +153,5 @@ async def test_get_instance_not_found(
         },
     )
 
-    response = await api_client.get("/extensions/EXT-1234-5678/instances/INS-1234-5678-0001")
-    assert response.status_code == 404
-
-
-@pytest.mark.asyncio
-async def test_delete_instance(
-    mocker: MockerFixture,
-    api_client: AsyncClient,
-):
-    mocker.patch(
-        "mrok.controller.routes.fetch_instance_or_404",
-        return_value={"name": "ins-1234-5678.ext-1234-5678"},
-    )
-    mocker.patch(
-        "mrok.controller.routes.unregister_instance",
-    )
-
-    response = await api_client.delete("/extensions/EXT-1234-5678/instances/INS-1234-5678-0001")
-    assert response.status_code == 204
-
-
-@pytest.mark.asyncio
-async def test_delete_extension_not_found(
-    mocker: MockerFixture,
-    api_client: AsyncClient,
-    settings_factory: SettingsFactory,
-    httpx_mock: HTTPXMock,
-):
-    settings = settings_factory()
-    mocker.patch(
-        "mrok.controller.routes.fetch_extension_or_404", return_value={"name": "ext-1234-5678"}
-    )
-
-    query = quote(
-        '(id="INS-1234-5678-0001.ext-1234-5678" or name="ins-1234-5678-0001.ext-1234-5678") '
-        f"and tags.{MROK_VERSION_TAG_NAME} != null"
-    )
-    httpx_mock.add_response(
-        method="GET",
-        url=f"{settings.ziti.api.management}/edge/management/v1/identities?filter={query}",
-        json={
-            "meta": {"pagination": {"totalCount": 0}},
-            "data": [],
-        },
-    )
-
-    response = await api_client.delete("/extensions/EXT-1234-5678/instances/INS-1234-5678-0001")
+    response = await api_client.get("/instances/INS-1234-1234-0001.ext-1234-1234")
     assert response.status_code == 404
